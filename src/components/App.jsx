da9e64c677f18db2120d695b78034f27244e6f02
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-12-08 11:22:26 +0200

    Copy project

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+//import { Box } from '../components/Box';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  render() {
+    return <></>;
+  }
+}
+
+export default App;
